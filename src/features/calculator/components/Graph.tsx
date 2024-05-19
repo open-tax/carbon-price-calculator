3ca80c2b36137f8fb5ccfdad20e4d332090e2aca
Julien <jbertazzolambert@gmail.com>
2024-05-19 18:04:35 -0400

    feat: more mobile improvements

--- src/features/calculator/components/Graph.tsx
+++ src/features/calculator/components/Graph.tsx
@@ -11,24 +11,36 @@ const currencyFormatter = (v: number | null) =>
     ? Intl.NumberFormat("en-US", {
         style: "currency",
         currency: "CAD",
-      }).format(v)
+        currencyDisplay: "narrowSymbol",
+        maximumFractionDigits: 0,
+      })
+        .format(v)
+        .replaceAll(",", "")
     : "";
 
 export function Graph(props: GraphProps) {
-  useEffect(() => console.log("Whats", props.data), [props.data]);
-
   return (
     <div className="h-1/2 md:absolute md:top-0 md:left-0 w-screen md:h-screen z-10">
       <LineChart
         colors={cheerfulFiestaPalette}
-        series={[{ data: props.data, valueFormatter: currencyFormatter }]}
+        series={[
+          {
+            data: props.data,
+            valueFormatter: currencyFormatter,
+          },
+        ]}
         xAxis={[
           {
             data: [2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030],
-            valueFormatter: (v) =>
-              `${v.toString()}/${((v + 1) % 100).toString()}`,
+            valueFormatter: (v) => (v % 1 === 0 ? `${v.toString()}` : ""),
           },
         ]}
+        yAxis={[{ valueFormatter: currencyFormatter }]}
+        sx={{
+          "& .MuiMarkElement-root": {
+            fill: "#174b80",
+          },
+        }}
       />
     </div>
   );
